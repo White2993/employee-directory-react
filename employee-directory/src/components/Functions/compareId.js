dd5d89fbd5b1f93e3f65aac13c03990d874c5c40
White2993 <47195867+White2993@users.noreply.github.com>
2020-01-20 19:51:23 -0600

    Changed files a bit

---
+++ employee-directory/src/components/Functions/compareId.js
@@ -0,0 +1,12 @@
+export default     function compare(a, b) {
+  const nameA = a.id
+  const nameB = b.id
+
+  let comparison = 0;
+  if (nameA > nameB) {
+    comparison = 1;
+  } else if (nameA < nameB) {
+    comparison = -1;
+  }
+  return comparison;
+}
